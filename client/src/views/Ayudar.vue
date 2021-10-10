c4ba362f024666a6525d23e974a59df87b784961
Diana Sofía <dsmartinezt@gmail.com>
2021-10-10 12:32:39 -0500

    Cambios enlaces <a> en ayudar.vue

--- client/src/views/Ayudar.vue
+++ client/src/views/Ayudar.vue
@@ -1,10 +1,9 @@
 <template>
   <BasicLayouts>
-<!-- INICIO Cómo ayudar-->
+    <!-- INICIO Cómo ayudar-->
     <main>
-
       <h1 class="title">
-         <span class="title__span">¿</span>
+        <span class="title__span">¿</span>
         <span class="title__span">C</span>
         <span class="title__span">Ó</span>
         <span class="title__span">M</span>
@@ -17,40 +16,49 @@
         <span class="title__span">A</span>
         <span class="title__span">R</span>
         <span class="title__span">?</span>
-               </h1>
+      </h1>
 
       <section class="contenedor-ayudar sombra">
-
         <div class="entrada-ayudar">
           <div class="alinear-centro">
-            <img class="imagen-redonda " src="../assets/ayudar4.jpeg" alt="">
+            <img class="imagen-redonda" src="../assets/ayudar4.jpeg" alt="" />
           </div>
 
           <h2>¡Súmate a nuestro equipo!</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. A nisi placeat temporibus maiores quis explicabo
-            qui iure libero quasi. Molestias facilis alias a provident mollitia consequatur assumenda dolore nobis unde? <br> <br>
-            <a href="#" class="saber__mas">Quiero saber más...</a></p>
-         
-          
-          <div class="alinear-centro">
-            <a class="boton" href="/register">Registrarme</a>
-          </div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A nisi
+            placeat temporibus maiores quis explicabo qui iure libero quasi.
+            Molestias facilis alias a provident mollitia consequatur assumenda
+            dolore nobis unde? <br />
+            <br />
+            <a href="#" class="saber__mas">Quiero saber más...</a>
+          </p>
 
+          <div class="alinear-centro">
+            <router-link class="boton" to="/register">
+            Registrarme
+            </router-link>  
+            
+          </div>
         </div>
 
         <div class="entrada-ayudar">
           <div class="alinear-centro">
-            <img class="imagen-redonda" src="../assets/ayudar6.jpg" alt="">
+            <img class="imagen-redonda" src="../assets/ayudar6.jpg" alt="" />
           </div>
 
           <h2>¡Apadrina una especie!</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum labore qui magni, eum ullam excepturi et
-            alias perspiciatis reiciendis atque ipsum placeat autem impedit! Nisi accusantium quibusdam molestiae quia
-            rerum! <br><br><a href="#" class="saber__mas">Quiero saber más...</a>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+            labore qui magni, eum ullam excepturi et alias perspiciatis
+            reiciendis atque ipsum placeat autem impedit! Nisi accusantium
+            quibusdam molestiae quia rerum! <br /><br /><a
+              href="#"
+              class="saber__mas"
+              >Quiero saber más...</a
+            >
           </p>
-          
+
           <div class="alinear-centro">
             <a class="boton" href="#">Apadrinar</a>
           </div>
@@ -58,22 +66,22 @@
 
         <div class="entrada-ayudar">
           <div class="alinear-centro">
-            <img class="imagen-redonda" src="../assets/ayudar.jpg" alt="">
+            <img class="imagen-redonda" src="../assets/ayudar.jpg" alt="" />
           </div>
 
           <h2>¡Haz una donación!</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio maxime suscipit molestias iure sit
-            temporibus veniam tempore accusamus, aliquid rem aspernatur perferendis numquam nesciunt rerum consequatur
-            est vero sapiente harum? <br><br> <a href="#" class="saber__mas">Quiero saber más...</a>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+            maxime suscipit molestias iure sit temporibus veniam tempore
+            accusamus, aliquid rem aspernatur perferendis numquam nesciunt rerum
+            consequatur est vero sapiente harum? <br /><br />
+            <a href="#" class="saber__mas">Quiero saber más...</a>
           </p>
-          
+
           <div class="alinear-centro">
             <a class="boton" href="#">Donar</a>
           </div>
-
         </div>
-
       </section>
 
       <div class="compartir-ayudar">
@@ -81,17 +89,37 @@
         <div class="compartir-redes">
           <div class="iconos-redes">
             <a href="https://www.facebook.com/IsnosBirding">
-              <svg xmlns="http://www.w3.org/2000/svg" class="icon-ayudar icon-tabler icon-tabler-brand-facebook" width="44"
-                height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff2825" fill="none" stroke-linecap="round"
-                stroke-linejoin="round">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="icon-ayudar icon-tabler icon-tabler-brand-facebook"
+                width="44"
+                height="44"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="#ff2825"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+                <path
+                  d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3"
+                />
               </svg>
             </a>
             <a href="https://www.instagram.com/isnosbirding/">
-              <svg xmlns="http://www.w3.org/2000/svg" class="icon-ayudar icon-tabler icon-tabler-brand-instagram" width="44"
-                height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff2825" fill="none" stroke-linecap="round"
-                stroke-linejoin="round">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="icon-ayudar icon-tabler icon-tabler-brand-instagram"
+                width="44"
+                height="44"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="#ff2825"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <rect x="4" y="4" width="16" height="16" rx="4" />
                 <circle cx="12" cy="12" r="3" />
@@ -99,49 +127,62 @@
               </svg>
             </a>
             <a href="#">
-              <svg xmlns="http://www.w3.org/2000/svg" class="icon-ayudar icon-tabler icon-tabler-brand-twitter" width="44"
-                height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff2825" fill="none" stroke-linecap="round"
-                stroke-linejoin="round">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="icon-ayudar icon-tabler icon-tabler-brand-twitter"
+                width="44"
+                height="44"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="#ff2825"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path
-                  d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z" />
+                  d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z"
+                />
               </svg>
             </a>
             <a href="#">
-              <svg xmlns="http://www.w3.org/2000/svg" class="icon-ayudar icon-tabler icon-tabler-brand-whatsapp" width="44"
-                height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff2825" fill="none" stroke-linecap="round"
-                stroke-linejoin="round">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="icon-ayudar icon-tabler icon-tabler-brand-whatsapp"
+                width="44"
+                height="44"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="#ff2825"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
                 <path
-                  d="M9 10a0.5 .5 0 0 0 1 0v-1a0.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a0.5 .5 0 0 0 0 -1h-1a0.5 .5 0 0 0 0 1" />
+                  d="M9 10a0.5 .5 0 0 0 1 0v-1a0.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a0.5 .5 0 0 0 0 -1h-1a0.5 .5 0 0 0 0 1"
+                />
               </svg>
             </a>
           </div>
         </div>
       </div>
-
-
-
     </main>
 
-
     <!-- FIN cómo ayudar-->
-
   </BasicLayouts>
 </template>
 
 <script>
-import BasicLayouts from '../layouts/BasicLayouts.vue'
+import BasicLayouts from "../layouts/BasicLayouts.vue";
 export default {
-    name:'Ayudar',
+  name: "Ayudar",
   components: { BasicLayouts },
-
-}
+};
 </script>
 
 <style>
-
 /*INCIO CÓMO AYUDAR*/
 
 p {
@@ -153,7 +194,7 @@ img {
   max-width: 100%;
 }
 
-a.boton{
+a.boton {
   text-decoration: none;
   color: #fff;
 }
@@ -180,14 +221,13 @@ a.boton{
 
 .contenedor-ayudar h2 {
   text-align: center;
- 
 }
 
 @media (min-width: 768px) {
   .contenedor-ayudar {
     display: flex;
     justify-content: space-between;
-     margin-top: 60px;
+    margin-top: 60px;
   }
 }
 
@@ -253,15 +293,12 @@ h3 {
   margin-bottom: 2rem;
 }
 
-.compartir-ayudar{
-    margin-top: 60px;
+.compartir-ayudar {
+  margin-top: 60px;
 }
 
-a{
+a {
   text-decoration: none;
 }
 /*FIN CÓMO AYUDAR*/
-
-
-
 </style>
